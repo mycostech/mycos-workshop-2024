a1cdd07b729dbc78f3abce4dddfe07f94eb6324c
Sahapap <sahapap_panya558@outlook.co.th>
2024-08-23 18:43:26 +0700

    initial score page.

--- src/pages/StartPage/index.tsx
+++ src/pages/StartPage/index.tsx
@@ -1,16 +1,30 @@
-import { Box, Button, Grid, TextField, Typography } from "@mui/material"
+import { Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material"
 import { useState } from "react"
 import { useMain } from "../../contexts/MainContext"
 import { useNavigate } from 'react-router-dom'
+import ScoreboardButton from "../../components/ScoreboardButton"
+
+const GAME_GUID = "c551724b-c8ab-4255-a131-3ac90ec9d0e2"
 
 const StartPage = () => {
-    const { setUser } = useMain()
+    const { setUser, setChannel, joinGame } = useMain()
     const navigate = useNavigate()
     const [name, setName] = useState<string>("John Doe")
+    const [channelName, setChannelName] = useState<string>(GAME_GUID)
 
     const onStartBtnClick = () => {
         setUser(name)
-        navigate('/game')
+        joinGame(GAME_GUID)
+        setChannel(channelName)
+        navigate('/game', {state: {
+
+        }})
+    }
+
+    const onScoreboardClick = () => {
+        setChannel(GAME_GUID)
+        joinGame(GAME_GUID)
+        navigate('/score')
     }
 
     return(
@@ -18,6 +32,21 @@ const StartPage = () => {
             <Grid container>
                 <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
                     <Typography variant="h1">Color Dot Game</Typography>
+                </Grid>
+                <Grid item xl={12} lg={12} md={12} sm={12} xs={12} p={2}>
+                    <Typography variant="h2" p={1}>
+                        Select Channel
+                    </Typography>
+                    <FormControl fullWidth>
+                        <InputLabel>Channel</InputLabel>
+                        <Select
+                            value={channelName}
+                            label="Channel"
+                            onChange={e => setChannelName(e.target.value)}
+                        >
+                            <MenuItem value={GAME_GUID}>{GAME_GUID}</MenuItem>
+                        </Select>
+                    </FormControl>
                 </Grid>
                 <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
                     <TextField
@@ -32,6 +61,11 @@ const StartPage = () => {
                     <Button variant="contained" onClick={onStartBtnClick}>
                         Start
                     </Button>
+                </Grid>
+                <Grid item xl={12} lg={12} md={12} sm={12} xs={12} pt={1}>
+                    <ScoreboardButton
+                        onScoreboardClick={onScoreboardClick}
+                    />
                 </Grid>
             </Grid>
         </Box>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StartPage from './StartPage';
 import GamePage from './GamePage';
+import ScorePage from './ScorePage';
 
 const MainPage = () => {
     return(
@@ -8,6 +9,7 @@ const MainPage = () => {
             <Routes>
                 <Route path='/' element={<StartPage/>} />
                 <Route path='/game' element={<GamePage/>} />
+                <Route path='/score' element={<ScorePage/>}/>
             </Routes>
         </Router>
     )
