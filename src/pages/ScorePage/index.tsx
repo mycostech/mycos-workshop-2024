9711009bda4ee5c327597383934a7a38154daa76
Sahapap <sahapap_panya558@outlook.co.th>
2024-08-29 21:54:04 +0700

    Add logic

--- src/pages/ScorePage/index.tsx
+++ src/pages/ScorePage/index.tsx
@@ -1,20 +1,27 @@
-import { useEffect } from "react"
-import { Box, Grid, Typography } from "@mui/material"
+import { useEffect, useState } from "react"
+import { Box, Button, Grid, Typography } from "@mui/material"
 import { useNavigate } from "react-router-dom"
 import { useMain } from "../../contexts/MainContext"
+import ChannelDropdown, { FIRST_OPTION_VALUE_DROPDOWN } from "../../components/ChannelDropdown"
+import { getAllListChannel, getScoreByAppName } from "../../api/appScore"
+import DataTable from "./Table"
 
 interface IScoreList{[key: string]: number}
 
 export default function ScorePage(){
     const navigate = useNavigate()
     
-    const { } = useMain()
+    const { joinGame, scoreList: wsScoreList } = useMain()
+
     // TO DO: Create new State
     // Create state for contain list of channels (string[])
+    const [appNameLists, setAppNameLists] = useState<string[]>([])
 
     // Create state for contain channel choice selected (string)
+    const [channelId, setChannelId] = useState<string | null>(FIRST_OPTION_VALUE_DROPDOWN)
     
     // Create state for contain score (type: IScoreList)
+    const [scoreList, setScoreList] = useState<IScoreList>({})
 
     useEffect(() => {
         // TO DO:
@@ -23,20 +30,41 @@ export default function ScorePage(){
             call getAllListChannel() function to get all channel list.
             And set to state.
         */
-       
+        async function GetAllListChannel() {
+            try {
+                const data = await getAllListChannel()
+                setAppNameLists(() => [...data])
+            } catch (error) {
+                console.error(error)
+                alert("Error")
+            }
+        }
+
+        GetAllListChannel()
     }, [])
 
     useEffect(() => {
         //TO DO:
         // Update realtime score from WS by set to score list state.
-        
-    }, [])
+        setScoreList(wsScoreList)
+    }, [wsScoreList])
 
 
     const onSelectChannel = async(channelIdSelected: string) => {
         // TO DO:
         // Function for get score by channel
         // and render score in table
+        if(channelIdSelected !== FIRST_OPTION_VALUE_DROPDOWN){
+            setChannelId(channelIdSelected)
+            try {
+                const res = await getScoreByAppName(channelIdSelected)
+                setScoreList(res)
+                joinGame(channelIdSelected)
+            } catch (error) {
+                console.error(error)
+                alert(`Can't get score from server.`)
+            }
+           }
     }
 
     return(
@@ -52,12 +80,28 @@ export default function ScorePage(){
                     TO DO:
                     Render channel dropdown 
                    */}
+                    <ChannelDropdown
+                        appNameLists={appNameLists}
+                        channelName={channelId || ""}
+                        onChange={(val) => onSelectChannel(val)}
+                    />
                 </Grid>
                 <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
                     {/*TO DO: Render data table for show score */}
+                    <DataTable
+                        data={scoreList}
+                    />
                 </Grid>
                 <Grid item xl={12} lg={12} md={12} sm={12} xs={12} p={1}>
                     {/*TO DO: Render button for back to home */}
+                    <Button
+                        variant="contained"
+                        onClick={() => {
+                            navigate('/')
+                        }}
+                    >
+                        Back to Home
+                    </Button>
                 </Grid>
             </Grid>
         </Box>
