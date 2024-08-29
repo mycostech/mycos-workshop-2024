7c056d12364485071db8b9fa8c0b272af4e6c3e5
Sahapap <sahapap_panya558@outlook.co.th>
2024-08-29 21:49:39 +0700

    Prepare code for training (score page)

--- src/pages/ScorePage/index.tsx
+++ src/pages/ScorePage/index.tsx
@@ -26,6 +26,13 @@ export default function ScorePage(){
        
     }, [])
 
+    useEffect(() => {
+        //TO DO:
+        // Update realtime score from WS by set to score list state.
+        
+    }, [])
+
+
     const onSelectChannel = async(channelIdSelected: string) => {
         // TO DO:
         // Function for get score by channel
